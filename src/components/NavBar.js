7a25fc4eef312d893b2c8b271793a093e3735cc6
Angie Turne <angie.turne@hotmail.com>
2021-10-21 15:21:55 -0300

    agrego navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+const NavBar = () => {
+  return (
+    <div>
+      <h4>TIENDA</h4>
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Servicios</a>
+        </li>
+        <li>
+          <a href="#">Contacto</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
