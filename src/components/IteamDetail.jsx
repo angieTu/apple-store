602019ff6471682b61ab0f07e9662fd096bb0868
Angie Turne <angie@MacBook-Air-de-Angie.local>
2021-11-10 18:20:03 -0300

    detalle del producto

--- src/components/IteamDetail.jsx
+++ src/components/IteamDetail.jsx
@@ -1,17 +1,21 @@
-import { Card, Container } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import ItemCount from "./ItemCount";
 
 const IteamDetail = ({ prod }) => {
   return (
-    <Card className="text-center">
-      <Card.Header>{prod.title}</Card.Header>
-      <Card.Body>
-        <Card.Title>${prod.price}</Card.Title>
-        <Card.Text>{prod.description}</Card.Text>
-        <ItemCount stock={prod.stock} />
-      </Card.Body>
-      <Card.Footer className="text-muted"> {prod.stock} stock</Card.Footer>
-    </Card>
+    <>
+      {prod && (
+        <Card className="text-center">
+          <Card.Header>{prod.title}</Card.Header>
+          <Card.Body>
+            <Card.Title>${prod.price}</Card.Title>
+            <Card.Text>{prod.description}</Card.Text>
+            <ItemCount stock={prod.stock} />
+          </Card.Body>
+          <Card.Footer className="text-muted">{prod.stock} stock</Card.Footer>
+        </Card>
+      )}
+    </>
   );
 };
 
--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,22 +1,24 @@
 import ItemDetail from "./IteamDetail";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Container } from "react-bootstrap";
+
 import productos from "../data/data";
 
 const getItem = new Promise((res, rej) => {
   setTimeout(() => {
-    res(productos[1]);
+    res(productos);
   }, 2000);
 });
 
 const ItemDetailContainer = () => {
-  const [detail, setDetail] = useState({});
-
-  getItem.then((res) => setDetail(res));
+  const [detail, setDetail] = useState([]);
+  useEffect(() => {
+    getItem.then((res) => setDetail(res));
+  }, []);
 
   return (
     <Container>
-      <ItemDetail prod={detail} />
+      <ItemDetail prod={detail[1]} />
     </Container>
   );
 };
